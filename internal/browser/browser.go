98bb9dba83833abce0fa8e1d7c401f05a15dca40
Antosik <grig_anton96@mail.ru>
2022-07-14 22:19:39 +0300

    feat(internal/browser): update browser args, add comments

--- internal/browser/browser.go
+++ internal/browser/browser.go
@@ -1,3 +1,4 @@
+// Package browser is the utility package that allows to launch browser with the perfomance oriented arguments
 package browser
 
 import (
@@ -10,27 +11,31 @@ func NewBrowser() *rod.Browser {
 	u := launcher.New().
 		Bin(path).
 		Set("--allow-running-insecure-content").
-		Set("--autoplay-policy", "user-gesture-required").
+		Set("--autoplay-policy", "user-gesture-required"). // Don't render video
 		Set("--disable-component-update").
-		Set("--disable-domain-reliability").
+		Set("--disable-domain-reliability"). // Disables Domain Reliability Monitoring.
 		Set("--disable-features", "AudioServiceOutOfProcess,IsolateOrigins,site-per-process").
-		Set("--disable-print-preview").
-		Set("--disable-setuid-sandbox").
-		Set("--disable-speech-api").
-		Set("--disable-web-security").
+		Set("--disable-extensions").     // Disable all chrome extensions
+		Set("--disable-notifications").  // Disables the Web Notification and the Push APIs.
+		Set("--disable-setuid-sandbox"). // Disable the setuid sandbox (Linux only).
+		Set("--disable-speech-api").     // Disables the Web Speech API (both speech recognition and synthesis).
+		Set("--disable-web-security").   // Don't enforce the same-origin policy.
 		Set("--disk-cache-size", "33554432").
+		Set("--enable-automation"). // Disable a few things considered not appropriate for automation.
 		Set("--enable-features", "SharedArrayBuffer").
-		Set("--hide-scrollbars").
-		Set("--ignore-gpu-blocklist").
-		Set("--in-process-gpu").
-		Set("--mute-audio").
-		Set("--no-default-browser-check").
-		Set("--no-pings").
-		Set("--no-sandbox").
-		Set("--no-zygote").
-		Set("--use-gl", "angle").
-		Set("--use-angle", "swiftshader").
-		Set("--single-process").
+		Set("--headless").                 // Run in headless mode
+		Set("--hide-scrollbars").          // Hide scrollbars
+		Set("--ignore-gpu-blocklist").     // Ignores GPU blocklist.
+		Set("--in-process-gpu").           // Run the GPU process as a thread in the browser process.
+		Set("--mute-audio").               // Mute any audio
+		Set("--no-default-browser-check"). // Disables the default browser check.
+		Set("--no-first-run").             // Skip first run wizards
+		Set("--no-pings").                 // Don't send hyperlink auditing pings
+		Set("--no-sandbox").               // Disables the sandbox for all process types that are normally sandboxed.
+		Set("--no-zygote").                // Disables the use of a zygote process for forking child processes.
+		Set("--use-gl", "angle").          // Select which implementation of GL the GPU process should use.
+		Set("--use-angle", "swiftshader"). // Select which ANGLE backend to use.
+		Set("--single-process").           // Runs the renderer and plugins in the same process as the browser.
 		MustLaunch()
 
 	return rod.New().ControlURL(u).MustConnect()
